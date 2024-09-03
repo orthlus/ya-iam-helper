f62a9a7c0409de29900da5c1cc9596afb359eb31
ort <orthodoxplus@ya.ru>
2024-09-03 19:11:54 +0300

    SupplierProperties

--- src/main/java/art/aelaort/DefaultSupplierProperties.java
+++ src/main/java/art/aelaort/DefaultSupplierProperties.java
@@ -1,31 +1,17 @@
 package art.aelaort;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+import java.net.URI;
+
+@Getter
+@Accessors(fluent = true)
+@AllArgsConstructor
 public class DefaultSupplierProperties implements SupplierProperties {
-	private String funcUrl;
+	private URI funcUrl;
 	private String funcSecret;
 	private String s3File;
 	private String s3Bucket;
-
-	public DefaultSupplierProperties(String funcUrl, String funcSecret, String s3File, String s3Bucket) {
-		this.funcUrl = funcUrl;
-		this.funcSecret = funcSecret;
-		this.s3File = s3File;
-		this.s3Bucket = s3Bucket;
-	}
-
-	public String funcUrl() {
-		return funcUrl;
-	}
-
-	public String funcSecret() {
-		return funcSecret;
-	}
-
-	public String s3File() {
-		return s3File;
-	}
-
-	public String s3Bucket() {
-		return s3Bucket;
-	}
 }
--- src/main/java/art/aelaort/SupplierProperties.java
+++ src/main/java/art/aelaort/SupplierProperties.java
@@ -1,7 +1,9 @@
 package art.aelaort;
 
+import java.net.URI;
+
 public interface SupplierProperties {
-	String funcUrl();
+	URI funcUrl();
 	String funcSecret();
 	String s3File();
 	String s3Bucket();
